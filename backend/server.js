84210e44ec3943d24494accd817d34e571913d14
Mohamed-MB-Begovic <mohamedmohamudabdulahiabdi@gmail.com>
2025-06-24 00:45:06 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const app = express();
  
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:9000',
+  origin: 'https://rental-3rsb.onrender.com',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
